2bcf31a1ef7541a8411caf36dad594d79e59a238
Nikolay Dorofeev <dorich2000@gmail.com>
2022-05-13 01:54:29 +0700

    Path prefix

--- gridsome.config.js
+++ gridsome.config.js
@@ -11,6 +11,7 @@ module.exports = {
     touchicon: './src/assets/favicon.png'
   },
   siteUrl: (process.env.SITE_URL ? process.env.SITE_URL : 'https://example.com'),
+  pathPrefix: process.env.PATH_PREFIX ? process.env.PATH_PREFIX: '',
   settings: {
     web: process.env.URL_WEB || false,
     twitter: process.env.URL_TWITTER || false,
